836e95703e0cac55baf3538c490e1b0f190020a3
mcharette-SF <1844680@csfoy.ca>
2024-04-16 16:21:18 -0400

    Added the .yml workflows

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "TP2 - Revue de Code",
   description: "Site pour la revue de code à chaque semaine.",
+  base: "/appweb-trpr02/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
