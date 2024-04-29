f1185cc434440e51917393870a5f2dbb74d58f6c
mcharette-SF <1844680@csfoy.ca>
2024-04-28 20:22:22 -0400

    Changement de place de API_URL dans services

--- src/services/shipsService.ts
+++ src/services/shipsService.ts
@@ -1,11 +1,5 @@
 import axios from 'axios'
-// import { API } from '@/shared/config'
-
-// Note sur le gestion des erreurs:
-// - Dans ce projet, la gestion des erreurs (try/catch) est gérée par le code qui utilise ce service.
-// - Dans un contexte d'entreprise, il serait utile de loguer les erreurs qui surviennent dans ce service et ensuite de les relancer en précisant le contexte de l'erreur.
-
-const API_URL = 'http://127.0.0.1:3000'
+import { API_URL } from './global'
 
 async function getShips () {
   const { data } = await axios.get(`${API_URL}/ships`);
