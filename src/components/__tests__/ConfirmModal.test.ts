67e67d484b897e3ab986634ae3b85048232d321b
Nowbowdy <zachary.vandermeerschen@gmail.com>
2024-04-28 21:13:35 -0400

    ajustement des tests du modal

--- src/components/__tests__/ConfirmModal.test.ts
+++ src/components/__tests__/ConfirmModal.test.ts
@@ -42,4 +42,33 @@ describe('ConfirmModal.vue', () => {
 
     expect(wrapper.emitted()).toHaveProperty('onModalConfirmed')
   })
+
+  it('Le style:display des boutons doivent être la valeur de leurs props', async () => {
+    const confirmButtonStyle = 'block' // Définissez la valeur du prop confirmButtonStyle à tester
+  
+    const wrapper = mount(ConfirmModal, {
+      props: {
+        title: 'mon titre',
+        body: 'mon contenu',
+        cancelButton: 'mon bouton annuler',
+        confirmButton: 'mon bouton confirmer',
+        cancelButtonStyle: 'block',
+        confirmButtonStyle: 'block'
+      }
+    })
+  
+    const confirmButton = wrapper.find('[name="confirmer"]')
+    const cancelmButton = wrapper.find('[name="annuler"]')
+
+  
+    expect(confirmButton.attributes('style')).toContain('display: block');
+    expect(cancelmButton.attributes('style')).toContain('display: block');
+
+    await wrapper.setProps({ cancelButtonStyle: 'none', confirmButtonStyle: 'none' });
+
+    expect(confirmButton.attributes('style')).toContain('display: none');
+    expect(cancelmButton.attributes('style')).toContain('display: none');
+
+  })
+
 })
